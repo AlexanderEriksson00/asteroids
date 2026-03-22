cdb59b32f0fd7f93df28bf4ddd8935b362278ec4
AlexanderEriksson00 <alexander-eriksson00@outlook.com>
2026-03-22 19:16:29 +0000

    2

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-
+from player import Player
 
 def main():
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    triangle = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        
-        
-        
-        
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        triangle.draw(screen)
+        triangle.update(dt)
+
         pygame.display.flip()
 if __name__ == "__main__":
     main()
